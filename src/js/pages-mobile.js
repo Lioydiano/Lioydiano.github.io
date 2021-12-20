40d582fe371176d4c3d1282bac1caabaa50938a4
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2021-12-20 23:38:07 +0100

    Add files via upload

--- src/js/pages-mobile.js
+++ src/js/pages-mobile.js
@@ -2,3 +2,56 @@ window.addEventListener('load', function() {
         document.getElementById('structP').innerHTML = `${localStorage.name}, puoi trovare la struttura del sito <a class="red-blue" href="tree.txt" target="_blank">qui</a>.`;
     }
 )
+
+document.getElementById('searchbar').addEventListener('focusin', function() {
+
+    let div = document.getElementsByClassName('searchbar-container')[0];
+    let ul = div.getElementsByTagName('ul')[0];
+
+    let pages = {
+        'mobile': 'Pagina principale',
+        'email-mobile': 'Contatti email',
+        'Il-Lioydiano-mobile': 'Primo articolo'
+    };
+
+    for (let page in pages) {
+        let li = document.createElement('li');
+        li.innerHTML = `<a href="${page}.html" target="_blank">${pages[page]}</a>`;
+        let a = li.getElementsByTagName('a')[0]
+        a.style.width = '100%';
+        ul.appendChild(li);
+    };
+})
+
+
+document.getElementById('searchbar').addEventListener('focusout', function() {
+    setTimeout(() => {
+        Array.from(document.getElementsByClassName('searchbar-container')[0].getElementsByTagName('ul')[0].getElementsByTagName('li'))
+        .forEach(li => {
+            li.remove();
+        })
+    }, 700);
+})
+
+
+function checkSearch() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchbar');
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName('searchbar-container')[0].getElementsByTagName('ul')[0];
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+document.getElementById('searchbar').addEventListener('keydown', checkSearch);
+document.getElementById('searchbar').addEventListener('keyup', checkSearch);
--- src/js/pages.js
+++ src/js/pages.js
@@ -1,4 +1,55 @@
 window.addEventListener('load', function() {
-        document.getElementById('structP').innerHTML = `${localStorage.name}, puoi trovare la struttura del sito <a class="red-blue" href="tree.txt" target="_blank">qui</a>.`;
+    document.getElementById('structP').innerHTML = `${localStorage.name}, puoi trovare la struttura del sito <a class="red-blue" href="tree.txt" target="_blank">qui</a>.`;
+})
+
+
+document.getElementById('searchbar').addEventListener('focusin', function() {
+
+    let div = document.getElementsByClassName('searchbar-container')[0];
+    let ul = div.getElementsByTagName('ul')[0];
+
+    let pages = {
+        'index': 'Pagina principale',
+        'email': 'Contatti email',
+        'Il-Lioydiano': 'Primo articolo'
+    };
+
+    for (let page in pages) {
+        let li = document.createElement('li');
+        li.innerHTML = `<a href="${page}.html" target="_blank">${pages[page]}</a>`;
+        ul.appendChild(li);
+    };
+})
+
+
+document.getElementById('searchbar').addEventListener('focusout', function() {
+    setTimeout(() => {
+        Array.from(document.getElementsByClassName('searchbar-container')[0].getElementsByTagName('ul')[0].getElementsByTagName('li'))
+        .forEach(li => {
+            li.remove();
+        })
+    }, 700);
+})
+
+
+function checkSearch() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchbar');
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName('searchbar-container')[0].getElementsByTagName('ul')[0];
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
     }
-)
+}
+
+
+document.getElementById('searchbar').addEventListener('keydown', checkSearch);
+document.getElementById('searchbar').addEventListener('keyup', checkSearch);
